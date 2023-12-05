57f71bc8b1f86ea7bad2dcc832965ee7e404bafa
Geoff Pado <geoff@pado.name>
2023-12-04 16:51:46 -0800

    Make Barcode more generic

---
+++ BarcodeGenerator/Sources/EAN/EANBarcode.swift
@@ -0,0 +1,33 @@
+//  Created by Geoff Pado on 12/4/23.
+//  Copyright © 2023 Cocoatype, LLC. All rights reserved.
+
+import ErrorHandling
+import SwiftUI
+
+struct EANBarcode: View {
+    private let encodedValue: [Bool]
+    init(value: String) {
+        do {
+            self.encodedValue = try EANValue(value).encodedValue
+        } catch {
+            ErrorHandling.fatalError(String(describing: error))
+        }
+    }
+
+    @Environment(\.displayScale) private var displayScale
+    var body: some View {
+        Path { path in
+            for i in 0..<encodedValue.count {
+                guard encodedValue[i] else { continue }
+                path.addRect(CGRect(x: i, y: 0, width: 1, height: encodedValue.count))
+            }
+        }
+        .frame(width: Double(encodedValue.count), height: Double(encodedValue.count))
+    }
+
+}
+
+#Preview {
+    EANBarcode(value: "4444444444444")
+        .previewLayout(.sizeThatFits)
+}
