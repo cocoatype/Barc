b18b05a7dd17a5018b7fe36518745afea592ae8d
Geoff Pado <geoff@pado.name>
2024-09-27 21:35:22 -0700

    Add error handling for missing codes in split view

--- Modules/WatchContents/Sources/WatchSplitView.swift
+++ Modules/WatchContents/Sources/WatchSplitView.swift
@@ -2,15 +2,22 @@
 //  Copyright © 2024 Cocoatype, LLC. All rights reserved.
 
 import Barcodes
+import ErrorHandling
 import SwiftUI
 
 struct WatchSplitView: View {
     @State private var selectedCode: Code?
     private let codes: [Code]
 
-    init(codes: [Code]) {
+    init(codes: [Code], errorHandler: any ErrorHandler = ErrorHandling.defaultHandler) {
         self.codes = codes
-        self.selectedCode = codes.first
+
+        if let selectedCode = codes.first {
+            self.selectedCode = selectedCode
+        } else {
+            self.selectedCode = nil
+            errorHandler.log(WatchSplitViewError.noSelectedCode, module: "WatchContents", type: "WatchSplitView")
+        }
     }
 
     var body: some View {
@@ -22,4 +29,8 @@ struct WatchSplitView: View {
             }
         }
     }
+}
+
+enum WatchSplitViewError: Error {
+    case noSelectedCode
 }
