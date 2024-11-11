60c33180180568e8d91a1f0666730d2fb0ca0084
Geoff Pado <geoff@pado.name>
2024-11-11 08:42:59 -0800

    Add "set date" intent

---
+++ Modules/Shortcuts/Sources/Intents/SetDateIntent.swift
@@ -0,0 +1,40 @@
+//  Created by Geoff Pado on 11/11/24.
+//  Copyright © 2024 Cocoatype, LLC. All rights reserved.
+
+import AppIntents
+import Barcodes
+import CoreLocation
+import Persistence
+
+struct SetDateIntent: AppIntent {
+    static let title: LocalizedStringResource = "SetDateIntent.title"
+    static let description: IntentDescription = "SetDateIntent.description"
+
+    static var parameterSummary: some ParameterSummary {
+        Summary("SetDateIntent.parameterSummary\(\.$code)\(\.$date)")
+    }
+
+    @Parameter(
+        title: "SetDateIntent.code"
+    )
+    var code: BarcodeEntity
+
+    @Parameter(
+        title: "SetDateIntent.date"
+    )
+    var date: Date
+
+    func perform() async throws -> some IntentResult & ReturnsValue<BarcodeEntity> {
+        let existingCode = code.code
+        let newCode = Code(name: existingCode.name, value: existingCode.value, location: existingCode.location, date: date)
+
+        let repository = Persistence.defaultRepository
+        try await repository.update(newCode)
+
+        return .result(value: BarcodeEntity(code: newCode))
+    }
+}
+
+enum SetDateIntentError: Error {
+    case missingCoordinate
+}
