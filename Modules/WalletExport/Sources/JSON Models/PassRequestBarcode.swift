6a17a242bb474c04ddf2b618ec178195d9a112a0
Geoff Pado <geoff@pado.name>
2024-12-13 19:39:22 -0800

    Create base64-encoded PDF417 Wallet request

--- Modules/WalletExport/Sources/JSON Models/PassRequestBarcode.swift
+++ Modules/WalletExport/Sources/JSON Models/PassRequestBarcode.swift
@@ -2,40 +2,47 @@
 //  Copyright © 2024 Cocoatype, LLC. All rights reserved.
 
 import Barcodes
+import Foundation
+import PDF417
 
 struct PassRequestBarcode: Encodable {
     let format: String
     let message: String
 
+    private init(format: String, message: String) {
+        self.format = format
+        self.message = message
+    }
+
     init(code: Code) throws {
         switch code.value {
         case .codabar(let value):
-            format = "codabar"
             let converter = CodabarElementToCharacterConverter()
             let characters = value.payload.elements.map(converter.character(for:))
-            self.message = String(characters)
+            self.init(format: "codabar", message: String(characters))
         case .code39(let value):
-            format = "code39"
             let converter = Code39ElementToCharacterConverter()
             let characters = value.payload.elements.map(converter.character(for:))
-            self.message = String(characters)
+            self.init(format: "code39", message: String(characters))
         case .code128(let value):
-            format = "code128"
             let converter = Code128ValueToStringConverter()
-            self.message = try converter.string(from: value)
+            try self.init(format: "code128", message: converter.string(from: value))
         case .ean(let value):
-            format = "ean13"
             let converter = EANDigitToCharacterConverter()
             let characters = value.payload.digits.map(converter.character(for:))
-            self.message = String(characters)
+            self.init(format: "ean13", message: String(characters))
         case .pdf417(let value):
-            #warning("FIX ME!!!")
-            fatalError()
+            let codewordConverter = CodewordToIntConverter()
+            let bytes = value.dataCodewords
+                .map(codewordConverter.value(for:))
+                .map(UInt16.init)
+                .map(\.bigEndian)
+                .flatMap { [UInt8($0 >> 8), UInt8($0 & 0xFF)] }
+            self.init(format: "pdf417", message: Data(bytes).base64EncodedString())
         case .qr(let value):
-            format = "qr"
             guard let message = String(data: value.payload.data, encoding: .utf8)
             else { throw PassRequestError.unrepresentableMessage }
-            self.message = message
+            self.init(format: "qr", message: message)
         }
     }
 }
