05ca64c3f91bf6592ae75b7fc6fef455616da1ae
Geoff Pado <geoff@pado.name>
2024-12-17 17:13:38 -0800

    Add thank you state to paywall entry in menu

--- Modules/Menu/Sources/Sections/Upsell/PaywallRouteCell.swift
+++ Modules/Menu/Sources/Sections/Upsell/PaywallRouteCell.swift
@@ -1,18 +1,40 @@
 //  Created by Geoff Pado on 9/6/24.
 //  Copyright © 2024 Cocoatype, LLC. All rights reserved.
 
+import Purchasing
 import SwiftUI
 
 struct PaywallRouteCell: View {
+    @State var hasUserBeenUnleashed: Bool
+
+    private let repository: any PurchaseRepository
+    init(repository: any PurchaseRepository = Purchasing.defaultRepository) {
+        self.repository = repository
+        _hasUserBeenUnleashed = State(initialValue: repository.cachedHasUserBeenUnleashed)
+    }
+
     var body: some View {
         NavigationLink(value: Route.paywall) {
             VStack(alignment: .leading) {
                 Text(MenuStrings.PaywallRouteCell.title)
                     .font(.title3)
                     .fontWeight(.black)
-                Text(MenuStrings.PaywallRouteCell.subtitle)
+                Text(subtitle)
                     .foregroundStyle(Color.secondary)
             }
+        }.task {
+            print("Fetching hasUserBeenUnleashed…")
+            do {
+                hasUserBeenUnleashed = try await repository.hasUserBeenUnleashed
+            } catch {}
+        }
+    }
+
+    private var subtitle: String {
+        if hasUserBeenUnleashed {
+            MenuStrings.PaywallRouteCell.purchasedSubtitle
+        } else {
+            MenuStrings.PaywallRouteCell.unpurchasedSubtitle
         }
     }
 }
